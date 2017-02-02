82c9ded75e203fead66e479a8c7926e65ec58f16
ZhanLiangUF <zliang@ufl.edu>
2017-02-01 20:05:37 -0500

    Better solution - less confusing

--- BinaryWatch.java
+++ BinaryWatch.java
@@ -12,15 +12,10 @@ public class BinaryWatch {
 
   }
   public List<String> readBinaryWatch(int num) {
-    List<String> times = new ArrayList<>();
-    for(int h=0;h<12;h++) {
-      for(int m=0;m<60;m++) {
-        //multiply by 64 to shift h 6 bits so it doesn't
-        if(Integer.bitCount(h*64 + m) == num) {
-          times.add(String.format("%d:%02d", h, m));
-        }
-      }
-    }
-    return times;
-  }
+      List<String> times = new ArrayList<>();
+      for (int h=0; h<12; h++)
+          for (int m=0; m<60; m++)
+              if (Integer.bitCount(h)+Integer.bitCount(m) == num)
+                  times.add(String.format("%d:%02d", h, m));
+      return times;  
 }
