509da0f62b93c720df98f684e63083a957a00a60
ZhanLiangUF <zliang@ufl.edu>
2017-02-01 19:58:04 -0500

    Finish binary watch

---
+++ BinaryWatch.java
@@ -0,0 +1,26 @@
+// A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).
+//
+// Each LED represents a zero or one, with the least significant bit on the right.
+//
+//
+// For example, the above binary watch reads "3:25".
+//
+// Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.
+import java.util.*;
+public class BinaryWatch {
+  public static void main(String[] args) {
+
+  }
+  public List<String> readBinaryWatch(int num) {
+    List<String> times = new ArrayList<>();
+    for(int h=0;h<12;h++) {
+      for(int m=0;m<60;m++) {
+        //multiply by 64 to shift h 6 bits so it doesn't
+        if(Integer.bitCount(h*64 + m) == num) {
+          times.add(String.format("%d:%02d", h, m));
+        }
+      }
+    }
+    return times;
+  }
+}
