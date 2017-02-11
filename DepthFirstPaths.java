ab5df9cc7e15c71ae8b95f5ad0635337201c7d22
ZhanLiangUF <zliang@ufl.edu>
2017-02-10 21:56:31 -0500

    finish DFS

---
+++ DepthFirstPaths.java
@@ -0,0 +1,23 @@
+//General DepthFirstPath, Recursive Implementation
+
+public class DepthFirstPaths {
+  private boolean[] marked;
+  private int[] edgeTo;
+  private int s;
+
+  public DepthFirstPaths(Graph G, int s) {
+    dfs(G, s);
+  }
+
+  private void dfs(Graph G, int v) {
+    marked[v] = true;
+    for(int w : G.adj(v)) {
+      if (!marked[w]) {
+        edgeTo[w] = v;
+        dfs(G,w);
+      }
+    }
+
+  }
+
+}
