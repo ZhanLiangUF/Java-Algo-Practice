5449262a7097eac95b365c3b6cd0cfb180d149f0
Han Liang <hanliang@Hans-MacBook-Air.local>
2017-05-14 22:02:09 -0400

    warmup

---
+++ removeDuplicates.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int a = 0;
+        for(int b = 1; b < nums.length; b++) {
+            if (nums[a] != nums[b]) {
+                nums[a+1] = nums[b];
+                a++;
+            }
+        }
+        return a+1;
+    }
+}
